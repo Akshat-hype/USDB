6251d89eac156fb74e7a5ada272613c6054cc3ce
KratiGupta04 <kratigupta0604@gmail.com>
2025-07-07 23:55:15 +0530

    Update lib.rs

--- src/btc_price_backend/src/lib.rs
+++ src/btc_price_backend/src/lib.rs
@@ -45,8 +45,11 @@ async fn get_btc_price() -> String {
 }
 
 /// crude string-based extractor to get value from: {"symbol":"BTCUSDT","price":"67123.45"}
+
 fn extract_price(json_str: &str) -> Option<String> {
+
     // look for: "price":" and extract till next "
+
     let key = "\"price\":\"";
     if let Some(start) = json_str.find(key) {
         let from = start + key.len();
