9bbcd06e7b67be0d289e03b4a1ca61c6dab4647e
Abhishek-tech471 <abhishek.yadav_cs.bct23@gla.ac.in>
2025-08-15 23:27:18 +0530

    some changes

--- src/notify_btc_recieved/src/lib.rs
+++ src/notify_btc_recieved/src/lib.rs
@@ -1,3 +1,21 @@
+use candid::{CandidType, Principal, candid_method};
+use ic_cdk_macros::update;
+use serde::Deserialize;
+use std::cell::RefCell;
+
+type UsdbAmount = u64;
+
+thread_local! {
+    static TOTAL_SUPPLY: RefCell<UsdbAmount> = RefCell::new(0);
+    static USER_BALANCES: RefCell<Vec<UserBalance>> = RefCell::new(Vec::new());
+}
+
+#[derive(CandidType, Deserialize, Clone)]
+struct UserBalance {
+    principal: Principal,
+    amount: UsdbAmount,
+}
+
 #[derive(CandidType, Deserialize)]
 struct BtcDepositNotification {
     btc_address: String,
@@ -8,6 +26,7 @@ struct BtcDepositNotification {
 }
 
 #[update]
+#[candid_method(update)]
 fn notify_btc_received(notification: BtcDepositNotification) -> String {
     let BtcDepositNotification {
         receiver,
@@ -15,11 +34,12 @@ fn notify_btc_received(notification: BtcDepositNotification) -> String {
         ..
     } = notification;
 
-    // Mint tokens to the user
+    // Increase total supply
     TOTAL_SUPPLY.with(|supply| {
         *supply.borrow_mut() += usdb_amount;
     });
 
+    // Update user balance
     USER_BALANCES.with(|balances| {
         let mut user_balances = balances.borrow_mut();
         if let Some(entry) = user_balances.iter_mut().find(|b| b.principal == receiver) {
